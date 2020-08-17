972eb9b62e4893dc99bb8f73403de27ec686108f
Mbote-Joseph <mbotejoseph001@gmail.com>
2020-08-17 15:03:19 +0300

    first commit

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -8,12 +8,13 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
   goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2019,9,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2019,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,11,18)),
-    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2019,3,14)),
+    new Goal(1, 'HTML and CSS', 'Learning HTML and CSS languages for web development',new Date(2020,1,14)),
+    new Goal(2,'Bootstrap','Using Bootstrap to make the frontend nice',new Date(2020,6,9)),
+    new Goal(3,'JavaScript','Learning Javascript to enhance interacting with the server Side',new Date(2020,6,9)),
+    new Goal(4,'Angular','Using a JavaScript framework which uses TypeScript for the frontend',new Date(2020,8,12)),
+    new Goal(5,'Python','Learning Python to help in Backend',new Date(2020,9,18)),
+    new Goal(6,'Django','Learning about Django which is a python framework',new Date(2020,10,14)),
+    new Goal(7,'FullStack web development','Integrating Angular and Django to perform fullstack tasks ',new Date(2021,1,1)),
   ];
 
   toggleDetails(index){
